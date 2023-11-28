87f0334eb6da30b74c07fc6efc1a464d28bc319e
StevOti <steveotieno5640@gmail.com>
2023-11-28 00:20:27 -0800

    Python programming

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,19 +8,22 @@
  */
 int check_cycle(listint_t *list)
 {
-        listint_t *slow = list;
-        listint_t *fast = list;
+	listint_t *slow, *fast;
 
-        if (!list)
-                return (0);
+	if (list == NULL || list->next == NULL)
+		return (0);
 
-        while (slow && fast && fast->next)
-        {
-                slow = slow->next;
-                fast = fast->next->next;
-                if (slow == fast)
-                        return (1);
-        }
+	slow = list->next;
+	fast = list->next->next;
 
-        return (0);
+	while (slow && fast && fast->next)
+	{
+		if (slow == fast)
+			return (1);
+
+		slow = list->next;
+		fast = list->next->next;
+	}
+
+	return (0);
 }

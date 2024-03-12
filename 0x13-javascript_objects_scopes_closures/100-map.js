cfe9e8c3f5a682fb5fd2ac9c94d30373b5fa74d5
StevOti <steveotieno5640@gmail.com>
2024-03-12 16:57:23 -0700

    Factor index

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,16 +1,4 @@
-const dict = require('./101-data').dict;
-
-const totalist = Object.entries(dict);
-const vals = Object.values(dict);
-const valsUniq = [...new Set(vals)];
-const newDict = {};
-for (const j in valsUniq) {
-  const list = [];
-  for (const k in totalist) {
-    if (totalist[k][1] === valsUniq[j]) {
-      list.unshift(totalist[k][0]);
-    }
-  }
-  newDict[valsUniq[j]] = list;
-}
-console.log(newDict);
+#!/usr/bin/node
+const list = require('./100-data.js').list;
+console.log(list);
+console.log(list.map((item, index) => item * index));
